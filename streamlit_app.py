e246861d6c9a17d514de9239b0d4d8d88ee5df82
l33th <l33th@protonmail.ch>
2022-04-16 07:17:02 +0200

    sales model example update in model

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 import streamlit as st
-import mymodel as m
+# import my_model as m
 
 st.write("""
 # Sales Model
 Below is a sales prediction example for customers.
 """)
 window = st.slider("Forecast Window (days)")
-st.write(m.run(window=window))
+# st.write(m.run(window=window))
